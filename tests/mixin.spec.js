6763e6b29ed266fcce9a40da506f4922ddc87661
Mbah Clinton <clintonmbah44@gmail.com>
2019-01-05 17:30:50 +0000

    added first test

--- tests/mixin.spec.js
+++ tests/mixin.spec.js
@@ -1,7 +1,36 @@
 import Mixin from '../src/mixin';
 
 describe('mixin', () => {
+	let _addChannel, _removeChannel, context;
+
+	beforeEach(() => {
+		_addChannel = jest.fn();
+		_removeChannel = jest.fn();
+
+		global.$options = {
+			channels: {
+				ChatChannel: {},
+				NotificationChannel: {}
+			}
+		};
+
+		global.$cable = {
+			_addChannel,
+			_removeChannel
+		};
+	});
+
 	test('It should correctly load channels on mount', () => {
-		expect(true).toBe(true);
+		Mixin.mounted.call(global);
+		expect(_addChannel).toBeCalledTimes(
+			Object.keys(global.$options.channels).length
+		);
+	});
+
+	test('It should correctly unsubscribe from channels on destroy', () => {
+		Mixin.destroyed.call(global);
+		expect(_removeChannel).toBeCalledTimes(
+			Object.keys(global.$options.channels).length
+		);
 	});
 });
