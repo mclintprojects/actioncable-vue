af7422708450290b893bc80aac086210e264e3d5
Mbah Clinton <clintonmbah44@gmail.com>
2019-01-05 05:25:28 +0000

    working on tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
 	moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
 	transform: {
-		'^.+\\.(js|jsx)?$': 'babel-jest'
+		'^.+\\.jsx?$': 'babel-jest'
 	},
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1'
 	},
 	testMatch: [
-		'<rootDir>/(tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
-	],
-	transformIgnorePatterns: ['<rootDir>/node_modules/']
+		'**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+	]
 };
