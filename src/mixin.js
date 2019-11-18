d38ef9aa82c641eaa681c038bde2ba3542dcc268
Mbah Clinton <clintonmbah44@gmail.com>
2019-11-18 19:53:34 +0000

    added support for dynamic channels

--- src/mixin.js
+++ src/mixin.js
@@ -5,7 +5,23 @@ export default {
 	mounted() {
 		if (this.$options.channels) {
 			Object.entries(this.$options.channels).forEach(entry => {
-				this.$cable._addChannel(entry[0], entry[1], this);
+				if (entry[0] != 'dynamic')
+					this.$cable._addChannel(entry[0], entry[1], this);
+				else {
+					const dynamicChannels = entry[1];
+					dynamicChannels.forEach(channel => {
+						const channelName = channel.channelName();
+						const channelObject = {
+							connected: channel['connected'],
+							rejected: channel['rejected'],
+							disconnected: channel['rejected'],
+							received: channel['received']
+						};
+
+						this.$options.channels[channelName] = channelObject;
+						this.$cable._addChannel(channelName, channelObject, this);
+					});
+				}
 			});
 		}
 	},
