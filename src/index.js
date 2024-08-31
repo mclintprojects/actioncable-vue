f0e6b104182d48bd8a576b4f8f3f9f7ca4a8f4e8
Clinton Mbah <clintonmbah44@gmail.com>
2024-08-31 10:38:55 +0000

    Refactor

--- src/index.js
+++ src/index.js
@@ -3,16 +3,32 @@ import Cable from "./cable";
 const ActionCableVue = {
   /**
    * ActionCableVue entry point
-   * @param Vue
+   * @param {Object} Vue - Vue instance
    * @param {Object} options - ActionCableVue options
    * @param {string|Function|null} [options.connectionUrl=null] - ActionCable server websocket URL
-   * @param {boolean} options.debug - Enable logging for debug
-   * @param {string} options.debugLevel - Debug level required for logging. Either `info`, `error`, or `all`
-   * @param {boolean} options.connectImmediately - Connect immediately or wait until the first subscription
-   * @param {object} options.store - Vuex store
+   * @param {boolean} [options.debug=false] - Enable logging for debug
+   * @param {string} [options.debugLevel='all'] - Debug level required for logging. Either `info`, `error`, or `all`
+   * @param {boolean} [options.connectImmediately=true] - Connect immediately or wait until the first subscription
+   * @param {Object} [options.store=null] - Vuex store
+   * @returns {Cable} - Cable instance
    */
   install(Vue, options) {
-    return new Cable(Vue, options);
+    const {
+      connectionUrl = null,
+      debug = false,
+      debugLevel = 'all',
+      connectImmediately = true,
+      store = null
+    } = options;
+
+    return new Cable(Vue, {
+      connectionUrl,
+      debug,
+      debugLevel,
+      connectImmediately,
+      store
+    });
   },
 };
+
 export default ActionCableVue;
