6e367bf2917fb2c4f3b251049012207549199b0b
Clinton Mbah <clintonmbah44@gmail.com>
2024-08-31 23:55:23 +0000

    Refactor mixin

--- src/mixin.js
+++ src/mixin.js
@@ -1,32 +1,39 @@
 import objectPolyfill from "./polyfill";
 
+function getChannels(context) {
+  return context.$ ? context.$.type.channels : (context.channels || context.$options.channels);
+}
+
+function hasChannels(context) {
+  return context.$options.channels || context.channels || (context.$ && context.$.type.channels);
+}
+
 function unsubscribe(context) {
-  if (context.$options.channels || context.channels || (context.$ && context.$.type.channels)) {
-    const channels = context.$ ? context.$.type.channels : (context.channels || context.$options.channels);
-    Object.entries(channels).forEach(([channelName, channelValue]) => {
-      if (channelName === "computed") {
-        channelValue.forEach((channel) => {
-          const computedChannelName = channel.channelName.call(context);
-          context.$cable._removeChannel(computedChannelName, context._uid);
-        });
-      } else {
-        context.$cable._removeChannel(channelName, context._uid);
-      }
-    });
-  }
+  if (!hasChannels(context)) return;
+
+  const channels = getChannels(context);
+
+  Object.entries(channels).forEach(([channelName, channelValue]) => {
+    if (channelName === "computed") {
+      channelValue.forEach((channel) => {
+        const computedChannelName = channel.channelName.call(context);
+        context.$cable._removeChannel(computedChannelName, context._uid);
+      });
+    } else {
+      context.$cable._removeChannel(channelName, context._uid);
+    }
+  });
 }
 
 function subscribe(context) {
-  if (context.$options.channels || context.channels || (context.$ && context.$.type.channels)) {
-    // polyfill Object.entries and Object.keys
-    objectPolyfill();
+  if (!hasChannels(context)) return;
 
-    const channels = context.$ ? context.$.type.channels : (context.channels || context.$options.channels);
+  objectPolyfill();
 
-    Object.entries(channels).forEach(entry => {
-      context.$cable._registerChannel(entry, context);
-    });
-  }
+  const channels = getChannels(context);
+  Object.entries(channels).forEach(entry => {
+    context.$cable._registerChannel(entry, context);
+  });
 }
 
 export default {
